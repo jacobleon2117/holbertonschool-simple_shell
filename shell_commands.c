0e0a0705a536cc80f4f7560316819b3378ed5f6f
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2023-12-12 10:16:53 -0600

    revert

--- shell_commands.c
+++ shell_commands.c
@@ -6,32 +6,47 @@
 #include <string.h>
 
 void execute_command(char *command) {
-    pid_t pid = fork();
+   char *args[MAX_INPUT_SIZE];
+   char *token;
+   int i = 0;
 
-    if (pid == 0) {
-        // Child process
-        execlp(command, command, (char *)NULL);
+   // Parse the command into a program name and arguments
+   token = strtok(command, " ");
+   while (token != NULL) {
+       args[i] = token;
+       i++;
+       token = strtok(NULL, " ");
+   }
+   args[i] = NULL;
 
-        // If execlp fails, print an error and exit
-        perror("Execution failed");
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        /* Fork failed */
-        perror("Fork failed");
-    } else {
-        /* Parent process */
-        int status;
-        waitpid(pid, &status, 0);
-/* Checks if the child process exited normally. This macro returns true if the child terminated normally. */
-        if (WIFEXITED(status)) { /* retrieves the exit status of the child process. */
-            int exit_status = WEXITSTATUS(status);
-            if (exit_status != 0) {
-                fprintf(stderr, "Command '%s' failed with exit status %d\n", command, exit_status);
-            }
-        } else if (WIFSIGNALED(status)) { /* If the child process did not exit normally, this condition checks if it terminated due to a signal. */
-            /* Prints an error message to stderr indicating that the command terminated by a signal. */
-            fprintf(stderr, "Command '%s' terminated by signal %d\n", command, WTERMSIG(status));
-        }
-    }
+   pid_t pid = fork();
+
+   if (pid == 0) {
+       // Child process
+       execvp(args[0], args);
+
+       // If execvp fails, print an error and exit
+       perror("Execution failed");
+       exit(EXIT_FAILURE);
+   } else if (pid < 0) {
+       /* Fork failed */
+       perror("Fork failed");
+   } else {
+       /* Parent process */
+       int status;
+       if (waitpid(pid, &status, 0) == -1) perror("waitpid");
+
+       /* Checks if the child process exited normally. This macro returns true if the child terminated normally. */
+       if (WIFEXITED(status)) { /* retrieves the exit status of the child process. */
+           int exit_status = WEXITSTATUS(status);
+           if (exit_status != 0) {
+               fprintf(stderr, "Command '%s' failed with exit status %d\n", command, exit_status);
+           }
+       } else if (WIFSIGNALED(status)) { /* If the child process did not exit normally, this condition checks if it terminated due to a signal. */
+           /* Prints an error message to stderr indicating that the command terminated by a signal. */
+           fprintf(stderr, "Command '%s' terminated by signal %d\n", command, WTERMSIG(status));
+       }
+   }
 }
+
 
