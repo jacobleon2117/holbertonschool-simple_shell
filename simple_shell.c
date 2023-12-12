7f0241d7a1e064566cb041675e9b788c1114b639
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2023-12-12 10:14:45 -0600

    reverting again

--- simple_shell.c
+++ simple_shell.c
@@ -1,36 +1,41 @@
 #include "shell_commands.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
 #define MAX_INPUT_SIZE 1024
 
 int main() {
-    char input[MAX_INPUT_SIZE];
+   char input[MAX_INPUT_SIZE];
 
-    while (1) {
-        // Display prompt and get user input
-        printf("$ ");
-        if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
-            // Handle end-of-file condition (Ctrl+D)
-            break;
-        }
+   while (1) {
+       printf("$ ");
+       if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
+           break;
+       }
 
-        // Remove newline character from input
-        size_t input_length = strlen(input);
-        if (input_length > 0 && input[input_length - 1] == '\n') {
-            input[input_length - 1] = '\0';
-        }
+       size_t input_length = strlen(input);
+       if (input_length > 0 && input[input_length - 1] == '\n') {
+           input[input_length - 1] = '\0';
+       }
 
-        // Exit the shell if the user enters 'exit' or 'quit'
-        if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) {
-            break;
-        }
+       if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) {
+           break;
+       }
 
-        // Execute the command
-        execute_command(input);
-    }
+       char *args[] = {input, NULL};
+       pid_t pid = fork();
 
-    return 0;
+       if (pid == -1) {
+           printf("Failed to fork.\n");
+           return 1;
+       } else if (pid == 0) {
+           execvp(args[0], args);
+       } else {
+           wait(NULL);
+       }
+   }
+
+   return 0;
 }
-
