d89e334f77de6f326f559abc854f369c682f890c
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2023-12-12 10:11:18 -0600

    revert

--- simple_shell.c
+++ simple_shell.c
@@ -9,27 +9,28 @@ int main() {
     char input[MAX_INPUT_SIZE];
 
     while (1) {
-        /* Display prompt and get user input */
+        // Display prompt and get user input
         printf("$ ");
         if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
-            /* Handle end-of-file condition (Ctrl+D) */
+            // Handle end-of-file condition (Ctrl+D)
             break;
         }
 
-        /* Remove newline character from input */
+        // Remove newline character from input
         size_t input_length = strlen(input);
         if (input_length > 0 && input[input_length - 1] == '\n') {
             input[input_length - 1] = '\0';
         }
 
-        /* Exit the shell if the user enters 'exit' or 'quit' */
+        // Exit the shell if the user enters 'exit' or 'quit'
         if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) {
             break;
         }
 
-        /* Execute the command */
+        // Execute the command
         execute_command(input);
     }
 
     return 0;
 }
+
